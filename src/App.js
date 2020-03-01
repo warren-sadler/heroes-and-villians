0104a9dfe65ba41d4c6a5f33ad4ab7e9fdca44e8
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-02-29 19:03:26 -0600

    starting ui

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from "react";
 function App() {
   return (
     <div className="w-screen h-screen bg-blue-400 flex items-center justify-center">
-      <h3 className="text-white font-bold text-xl">
-        Hello World From Tailwind
-      </h3>
+     <main className="card">
+       test
+     </main>
     </div>
   );
 }
